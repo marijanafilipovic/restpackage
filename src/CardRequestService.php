c7ddae397af6cf77be221c49b21d02d4739d60e9
Marijana <Marijana@remote1>
2021-12-20 17:21:13 +0100

    add ReadMe second commit

--- src/CardRequestService.php
+++ src/CardRequestService.php
@@ -5,27 +5,50 @@ namespace Marijana\Restpackage;
 
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
 
 class CardRequestService implements IRequestService
 {
     protected $client;
-    public function makeRequest($method, $url, $param)
+    public function makeRequest($method, $endpointName=null, $param=null)
     {
-        $url='https://countriesnow.space/api/v0.1/countries/states';
-        // $url='https://restcountries.com/v2/all';
-        $client = new Client();
-        $api_key = config('auth_app.AUTH-KEY');
-        // dd($api_key);
-        $headers = [
-            'AUTH-KEY' => $api_key
-        ];
-        $params = [];
-        $response = $client->request('GET', $url, [
-            //'json' => $params,
-            'headers' => $headers,
-            'verify'=>false,
-        ]);
-        $responseBody = json_decode($response->getBody());
-        return compact('responseBody');
-    }
+        //check endpoint from confing by endpointName and create endpointUrl
+
+        if($endpointName == ''){
+            $url='https://countriesnow.space/api/v0.1/countries';
+        }else{
+            $endUrl = config('endpoints.'.$endpointName);
+          //  dd($endUrl);
+
+           // $param = 150;
+            if(strpos($endUrl, ':id') !==false && (!empty($param))){
+                $endUrl = str_replace(':id', $param,$endUrl);
+            }
+            $baseURL = env('APP_URL');
+            $url = $baseURL.$endUrl;
+            //dd($url);
+        }
+        //check response
+
+                $client = new Client();
+                $api_key = config('auth_app.AUTH-KEY');
+                // dd($api_key);
+                $headers = [
+                    'AUTH-KEY' => $api_key
+                ];
+                //$params = [];
+        try{      $response = $client->request($method, $url, [
+                    //'json' => $params,
+                    'headers' => $headers,
+                    'verify'=>false,
+                ]);
+                 $responseBody = json_decode($response->getBody());
+                return compact('responseBody');
+            }catch(ClientException $ex){
+           // return $responseBody = $ex->getResponse()->getBody(true);
+               // return "TRY block did not response ";
+            }
+        }
+
 }
--- src/CountryRequestService.php
+++ src/CountryRequestService.php
@@ -5,27 +5,54 @@ namespace Marijana\Restpackage;
 
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
+use phpDocumentor\Reflection\Types\Null_;
 
 
 class CountryRequestService implements IRequestService
 {
 protected $client;
-public function makeRequest($method, $url, $param)
+public function makeRequest($method, $endpointName, $param)
 {
-    $url='https://restcountries.com/v2/all';
-    $client = new Client();
-    $api_key = config('auth_app.AUTH-KEY');
-    // dd($api_key);
-    $headers = [
-        'AUTH-KEY' => $api_key
-    ];
-    $params = [];
-    $response = $client->request($method, $url, [
-        //'json' => $params,
-        'headers' => $headers,
-        'verify'=>false,
-    ]);
-    $responseBody = json_decode($response->getBody());
-    return compact('responseBody');
+    //check endpoint from confing by endpointName and create endpointUrl
+
+    if($endpointName == ''){
+        $url='https://restcountries.com/v2/all';
+    }else{
+        $endUrl = config('endpoints.'.$endpointName);
+        //  dd($endUrl);
+
+        // $param = 150;
+        if(strpos($endUrl, ':id') !==false && (!empty($param))){
+            $endUrl = str_replace(':id', $param,$endUrl);
+        }
+        $baseURL = env('APP_URL');
+        $url = $baseURL.$endUrl;
+        //dd($url);
+    }
+    //check response
+    try{
+        $client = new Client();
+        $api_key = config('auth_app.AUTH-KEY');
+        // dd($api_key);
+        $headers = [
+            'AUTH-KEY' => $api_key
+        ];
+        //$params = [];
+        $response = $client->request($method, $url, [
+            //'json' => $params,
+            'headers' => $headers,
+            'verify'=>false,
+        ]);
+        if(!$response){
+            return "Bad request response.";
+        }
+        $responseBody = json_decode($response->getBody());
+        return compact('responseBody');
+    }catch(ClientException $ex){
+        dd($ex->getMessage());
+    }
 }
+
 }
