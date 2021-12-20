2562f793b5c1d99ba1ba4cef6afce770312de3f1
Marijana <Marijana@remote1>
2021-12-20 17:26:59 +0100

    refresh endpoints config

--- src/config/endpoints.php
+++ src/config/endpoints.php
@@ -3,5 +3,12 @@ return [
     'a list of the available countries' => '/countries',
     'get a specific country' => '/countries/:id',
     'get the card info' => '/cards/:id',
-    'gets the balance of the card' => '/cards/:id/balance'
+    'gets the balance of the card' => '/cards/:id/balance',
+    'gets the pin code of the card' => '/cards/:id/pin',
+    'get a list of transaction history, expects a timeframe' => '/cards/:id/history',
+    'creates a new debit card' => '/cards/create',
+    'deactivates a card'=> '/cards/:id/deactivate',
+    'activates a card' => '/cards/:id/activate',
+    'updates the pin of the card' => '/cards/:id/update',
+    'loads card with a balance, expects to get the amount to load as a parameter' => '/cards/:id/load'
 ];
